1a3f0424c4333e30b88acf916102b713777dc00f
Manuel <manuelcar96@outlook.com>
2024-12-30 13:49:46 -0500

    se crea boton de filtro para ver solo tareas completadas o todas las tareas

--- src/pages/TaskList.tsx
+++ src/pages/TaskList.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import client from '../api/client';
-import { Check, FilePenLine, Trash2 } from 'lucide-react';
-import { TriangleAlert } from 'lucide-react';
+import { Check, FilePenLine, Trash2, TriangleAlert } from 'lucide-react';
 
 interface Task {
   id: string;
@@ -14,6 +13,7 @@ const TaskList: React.FC = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedTaskId, setSelectedTaskId] = useState<string | null>(null);
+  const [showCompleted, setShowCompleted] = useState(false);
 
   // Obtener tareas al cargar el componente
   useEffect(() => {
@@ -73,25 +73,45 @@ const TaskList: React.FC = () => {
     setSelectedTaskId(null);
   };
 
+  const toggleCompletedTasks = () => {
+    setShowCompleted((prevState) => !prevState);
+  };
+
+  const filteredTasks = showCompleted
+    ? tasks.filter((task) => task.completed)
+    : tasks;
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-400 text-white px-4">
       <h1 className="text-4xl font-extrabold mb-6 text-center">
         Lista de <span className="text-gray-100">Tareas</span>
       </h1>
 
-      <button className="mb-4">
-        <a
-          href="/create"
-          className="px-4 py-2 bg-gray-600 text-white rounded-lg hover:bg-gray-700 font-semibold"
+      <div className="flex items-center space-x-4 mb-4">
+        <button
+          onClick={toggleCompletedTasks}
+          className={`px-4 py-2 rounded-lg font-semibold ${
+            showCompleted
+              ? 'bg-gray-600 text-white hover:bg-gray-700'
+              : 'bg-green-500 text-white hover:bg-green-600'
+          }`}
         >
-          Crear Tarea
-        </a>
-      </button>
+          {showCompleted ? 'Mostrar Todas' : 'Mostrar Completadas'}
+        </button>
+        <button>
+          <a
+            href="/create"
+            className="px-4 py-2 bg-gray-600 text-white rounded-lg hover:bg-gray-700 font-semibold"
+          >
+            Crear Tarea
+          </a>
+        </button>
+      </div>
 
       <div className="bg-white bg-opacity-10 backdrop-blur-md p-8 rounded-lg shadow-lg w-full max-w-2xl">
-        {tasks.length > 0 ? (
+        {filteredTasks.length > 0 ? (
           <ul className="space-y-4">
-            {tasks.map((task) => (
+            {filteredTasks.map((task) => (
               <li
                 key={task.id}
                 className="p-4 bg-white rounded-lg shadow-md flex flex-col sm:flex-row justify-between items-start sm:items-center"
@@ -135,15 +155,19 @@ const TaskList: React.FC = () => {
             ))}
           </ul>
         ) : (
-          <p className="text-gray-300 text-center">No hay tareas disponibles.</p>
+          <p className="text-gray-300 text-center">
+            {showCompleted
+              ? 'No hay tareas completadas.'
+              : 'No hay tareas disponibles.'}
+          </p>
         )}
       </div>
 
       {isModalOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white rounded-lg shadow-lg p-6 w-96">
-            <h1 className='text-2xl font-semibold text-red-500 mb-4 justify-center flex items-center'>
-              <TriangleAlert size={24} className='inline-block mr-2 ' />
+            <h1 className="text-2xl font-semibold text-red-500 mb-4 justify-center flex items-center">
+              <TriangleAlert size={24} className="inline-block mr-2" />
               Alerta
             </h1>
             <h2 className="text-lg font-bold mb-4 text-gray-800">
